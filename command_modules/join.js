13ca17011209c24b6b380f31c1a285c988d07054
freddyGiant <vincyfboling@gmail.com>
2020-12-12 22:11:48 -0800

    Update join.js

--- command_modules/join.js
+++ command_modules/join.js
@@ -1,6 +1,6 @@
 const join = (client, msg, args) => {
     if(args.length > 0)
-        joinChannelID(client, args[0]).then(s => console.log(s), console.log);
+        joinChannelID(client, args[0]).then(() => console.log("boop"));
     else
         joinUser(client, msg).then(s => console.log(s), s => console.log(s));
 };
@@ -10,16 +10,14 @@ const joinChannelID = (client, channelID) => {
     return new Promise(resolve => {
         channel = client.channels.cache.get(channelID);
 
-        const currentConnection = client.voice.connections.values().next().value;
-        
-        if(currentConnection !== undefined)
-        {    console.log(currentConnection);
-            if(channelID === currentConnection.toString())
-                resolve(`I'm already in ${channel.name}!`);
-        }
-        
+        const currentConnections = client.voice.connections;
+
+        // if we're already in the channel
+        if(currentConnections.size > 0 && channelID === currentConnections.values().next().value.channel.id)
+            resolve(`I'm already in ${channel.name}!`);
         // if the channel doesn't exist
-        if(!channel) resolve(`${channelID} is not the ID of a channel that exists!`);
+        else if(!channel) 
+            resolve(`${channelID} is not the ID of a channel that exists!`);
         else 
             // wait for authentication and resolve
             channel.join().then(connection => {
